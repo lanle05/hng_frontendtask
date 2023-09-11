994774019a91e988af793926ad1091e3bab74c3b
Omobolanle <omobolanle.shaibu@gmail.com>
2023-09-11 21:05:07 +0100

    finally

---
+++ server.js
@@ -0,0 +1,23 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  let filePath = path.join(__dirname, req.url === "/" ? "index.html" : req.url);
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404, { "Content-Type": "text/html" });
+      return res.end("404 Not Found");
+    }
+
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(data);
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
